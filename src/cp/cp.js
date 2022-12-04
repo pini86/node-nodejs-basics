bea0cd26901b25add938e3094c8b83236ad03a09
Mikhail Druzhinin <pini86@mail.ru>
2022-12-04 11:06:42 +0300

    feat: add wt,cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,12 @@
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+import { fork } from "child_process";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const pathFile = join(__dirname, "files", "script.js");
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  fork(pathFile, args);
 };
 
-spawnChildProcess();
+spawnChildProcess(["arguments1", "arguments2"]);
--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,32 @@
+import { Worker } from "worker_threads";
+import {cpus}  from "os";
+import path from "path";
+
+const workerPath = path.resolve(path.dirname(''), 'src', 'wt', 'worker.js')
+
+
 const performCalculations = async () => {
-    // Write your code here
+    let result = [];
+    cpus().forEach((v, i) => {
+        let worker = new Worker(workerPath, {workerData: 10 + i});
+        result.push(
+            new Promise(resolve => {
+                worker.on('message', obj => {
+                    resolve(result[i] = obj);
+                });
+                worker.on('error', () => {
+                    resolve(result[i] = {status: 'error', data: null});
+                });
+                worker.on('exit', (code) => {
+                    if (code) {
+                        reject(new Error(`Worker stopped with code ${code} exit.`));
+                    }
+                });
+            })
+        ) 
+    })
+    
+    console.log(await Promise.all(result));
 };
 
 await performCalculations();
